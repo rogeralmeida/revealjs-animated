d8fd89de649c00127fc825fb614d62abe1308c16
Roger Almeida <ralmeida@tyro.com>
2018-12-14 18:19:43 +1100

    added examples slides

--- test/animation-factory.test.js
+++ test/animation-factory.test.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const factory = require('../src/animation-factory');
-
 beforeAll(async() => {
-    page.goto('http://www.google.com');
+    await page.goto('http://www.google.com');
 });
 
 test('it should be able to create a move-to-middle-top animation', () => {
-    const element = page.document.documentElement.createElement('h1');
+    var document = page.document;
+    const element = document.documentElement.createElement('h1');
     expect(element).toBeDefined();
     element.setAttribute("data-animated-duration", "1000");
     element.setAttribute("data-animated-iterations", "1");
