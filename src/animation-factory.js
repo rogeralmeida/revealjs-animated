591dbe7bdaa069e14f6866f753a42b869762e954
Roger Almeida <ralmeida@tyro.com>
2018-12-17 20:10:35 +1100

    Reversing fragments

--- src/animation-factory.js
+++ src/animation-factory.js
@@ -1,7 +1,11 @@
 var AnimationFactory = (() => {
-    function middleTop(element) {
+    function middleTop(element, reverse=false) {
         var topDistance = (element.offsetTop + element.parentElement.offsetTop) * -1;
-        var keyframes = {transform: ["translate(0px, 0px)", `translate(0px, ${topDistance}px)`]};
+        var transform = ["translate(0px, 0px)", `translate(0px, ${topDistance}px)`];
+        if (reverse){
+            transform = [`translate(0px, ${topDistance}px)`, "translate(0px, 0px)"];
+        }
+        var keyframes = {transform: transform};
         var duration = Number(element.getAttribute("data-animated-duration"));
         var iterations = Number(element.getAttribute("data-animated-iterations"));
         var fill = element.getAttribute("data-animated-fill");
@@ -9,10 +13,14 @@ var AnimationFactory = (() => {
         return element.animate(keyframes, timing);
     }
 
-    function moveTo(element){
+    function moveTo(element, reverse=false){
         var topDistance = element.getAttribute("data-move-to-left");
         var leftDistance = element.getAttribute("data-move-to-top");
-        var keyframes = {transform: ["translate(0px, 0px)", `translate(${leftDistance}, ${topDistance})`]};
+        var transform = ["translate(0px, 0px)", `translate(${leftDistance}, ${topDistance})`];
+        if(reverse){
+            transform = [`translate(${leftDistance}, ${topDistance})`, "translate(0px, 0px)"];
+        }
+        var keyframes = {transform: transform};
         var duration = Number(element.getAttribute("data-animated-duration"));
         var iterations = Number(element.getAttribute("data-animated-iterations"));
         var fill = element.getAttribute("data-animated-fill");
@@ -20,14 +28,22 @@ var AnimationFactory = (() => {
         return element.animate(keyframes, timing);
     }
 
-    function scaleUp(element) {
+    function scaleUp(element, reverse=false) {
         var keyframes = {transform: ['scale(1, 1)', 'scale(2, 2)']};
+        if(reverse){
+            keyframes = {transform: ['scale(2, 2)', 'scale(1, 1)']};
+        }
         const duration = { duration: 1500, iterations: 1, fill: 'forwards' };
         return element.animate(keyframes, duration);
     }
 
-    function scaleDown(element) {
-        var keyframes = {transform: ['scale(1, 1)', 'scale(0.5, 0.5)']};
+    function scaleDown(element, reverse=false) {
+        var transform = ['scale(1, 1)', 'scale(0.5, 0.5)'];
+        if (reverse){
+            transform = ['scale(0.5, 0.5)', 'scale(1, 1)'];
+        } 
+        
+        var keyframes = {transform: transform};
         const duration = { duration: 1500, iterations: 1, fill: 'forwards' };
         return element.animate(keyframes, duration);
     }
@@ -42,6 +58,17 @@ var AnimationFactory = (() => {
                 return scaleUp(element);
             } else if (element.classList.contains('scale-down')){
                 return scaleDown(element);
+            }
+        },
+        revertAnimation: (element) => {
+            if (element.classList.contains('move-to-middle-top')) {
+                return middleTop(element, true);
+            } else if (element.classList.contains('move-to')) {
+                return moveTo(element, true);
+            } else if (element.classList.contains('scale-up')){
+                return scaleUp(element, true);
+            } else if (element.classList.contains('scale-down')){
+                return scaleDown(element, true);
             }
         }
     };
--- src/revealjs-animated.js
+++ src/revealjs-animated.js
@@ -3,6 +3,10 @@ var AnimationFactory = require('./animation-factory');
 RevealJsAnimated = (function () {
     return {
         initialize: function (revealjsInstance){
+            revealjsInstance.addEventListener( 'fragmenthidden', function( event ) {
+                var element = event.fragment;
+                AnimationFactory.revertAnimation(element);
+            } );
             revealjsInstance.addEventListener( 'fragmentshown', function( event ) {
                 var element = event.fragment;
                 AnimationFactory.buildAnimation(element);
