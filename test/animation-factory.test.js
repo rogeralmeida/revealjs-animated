a5802640d1725e7ce02d05b336f03b2d19cb93c8
Roger Almeida <ralmeida@tyro.com>
2018-12-17 20:14:46 +1100

    WIP adding puppeteer to run tests

--- test/animation-factory.test.js
+++ test/animation-factory.test.js
@@ -1,17 +1,29 @@
 'use strict';
 
 const factory = require('../src/animation-factory');
-beforeAll(async() => {
-    await page.goto('http://www.google.com');
-});
+const puppeteer = require('puppeteer');
 
-test('it should be able to create a move-to-middle-top animation', () => {
-    var document = page.document;
-    const element = document.documentElement.createElement('h1');
-    expect(element).toBeDefined();
-    element.setAttribute("data-animated-duration", "1000");
-    element.setAttribute("data-animated-iterations", "1");
-    element.setAttribute("data-animated-fill", "forwards");
-    var animation = factory.buildAnimation(element);
-    expect(animation.playState).toBe("paused");
+describe('Animation Factory', async () => {
+    var browser;
+    var page;
+    beforeAll(async () =>{
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+    });
+
+    afterAll(async () => {
+        browser.close();
+    });
+
+    it('should be able to create a move-to-middle-top animation', async () => {
+        await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
+        await page.pdf({path: 'hn.pdf', format: 'A4'});
+        // const element = document.documentElement.createElement('h1');
+        // expect(element).toBeDefined();
+        // element.setAttribute("data-animated-duration", "1000");
+        // element.setAttribute("data-animated-iterations", "1");
+        // element.setAttribute("data-animated-fill", "forwards");
+        // var animation = factory.buildAnimation(element);
+        // expect(animation.playState).toBe("paused");
+    });
 });
