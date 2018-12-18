92d1bb2eba857019a9bbdd8f26a8e11ca1cbc04c
Roger Almeida <ralmeida@tyro.com>
2018-12-18 23:27:09 +1100

    Converted it to a plugin

--- src/revealjs-animated.js
+++ src/revealjs-animated.js
@@ -1,24 +1,22 @@
 var AnimationFactory = require('./animation-factory');
 
-RevealJsAnimated = (function () {
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = `.reveal .slides section .fragment.animated {
-            opacity: 1;
-            visibility: visible;  
-        }`;
-    document.getElementsByTagName('head')[0].appendChild(style);
+var RevealJsAnimation = window.RevealJsAnimation || (() => {
+    if(typeof Reveal !== 'undefined'){
+        var style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = `.reveal .slides section .fragment.animated {
+                opacity: 1;
+                visibility: visible;
+            }`;
+        document.getElementsByTagName('head')[0].appendChild(style);
 
-    return {
-        initialize: function (revealjsInstance){
-            revealjsInstance.addEventListener( 'fragmenthidden', function( event ) {
-                var element = event.fragment;
-                AnimationFactory.buildAnimation(element, true);
-            } );
-            revealjsInstance.addEventListener( 'fragmentshown', function( event ) {
-                var element = event.fragment;
-                AnimationFactory.buildAnimation(element);
-            } );
-        }
-    };
+        Reveal.addEventListener( 'fragmenthidden', function( event ) {
+            var element = event.fragment;
+            AnimationFactory.buildAnimation(element, true);
+        } );
+        Reveal.addEventListener( 'fragmentshown', function( event ) {
+            var element = event.fragment;
+            AnimationFactory.buildAnimation(element);
+        } );
+    }
 })();
