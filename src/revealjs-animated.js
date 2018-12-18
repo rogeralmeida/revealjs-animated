04561d170e1c58c860c1bfa7e2091fa5a78e1865
Roger Almeida <ralmeida@tyro.com>
2018-12-18 22:51:33 +1100

    Moved the required CSS change to inside the library

--- src/revealjs-animated.js
+++ src/revealjs-animated.js
@@ -1,6 +1,14 @@
 var AnimationFactory = require('./animation-factory');
 
 RevealJsAnimated = (function () {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = `.reveal .slides section .fragment.animated {
+            opacity: 1;
+            visibility: visible;  
+        }`;
+    document.getElementsByTagName('head')[0].appendChild(style);
+
     return {
         initialize: function (revealjsInstance){
             revealjsInstance.addEventListener( 'fragmenthidden', function( event ) {
